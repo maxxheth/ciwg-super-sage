9ec638b13a2efd13a7dc16ac711f13daf88de45f
Maximillian Heth <info@edfx.co>
2025-10-02 16:40:15 -0500

    add CIWG"s custom WP toolchain for easier access / deployment

---
+++ wordpress-manager/backup-databases.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to log messages
+log_message() {
+  TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+  # ANSI escape codes for colors and formatting
+  BOLD_WHITE="\e[1;37m"
+  LIGHT_GRAY="\e[0;37m"
+  RESET="\e[0m"
+  
+  # Output the formatted log message
+  echo -e "${BOLD_WHITE}${TIMESTAMP}${RESET} - ${LIGHT_GRAY}$1${RESET}" | tee -a /var/log/wordpress-manager.log
+}
+
+BACKUP_DIR="/backups"
+DATE=$(date +%Y%m%d_%H%M%S)
+
+# Get a list of databases
+databases=$(mysql -h mysql -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
+
+for db in $databases; do
+  mysqldump -h mysql $db | gzip > $BACKUP_DIR/${db}_$DATE.sql.gz
+done
